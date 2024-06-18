06815959259896530309d0513fa4915b1c154fc1
Rodrigo Bernardo Medeiros <rodrigobernardo.medeiros@halliburton.com>
2024-06-18 18:08:44 -0300

    Clean main branch

--- src/api/dependencies/route_dependencies.py
+++
@@ -1,9 +0,0 @@
-def get_session():
-    """FastAPI Dependency to inject  sqlalchemy session object"""
-    from src.mappers.database import SessionLocal
-
-    session = SessionLocal()
-    try:
-        yield session
-    finally:
-        session.close()
